75b102c43e0383d6cda4cfd42612b2b870c36f68
Jarrod Folino <jarrod.folino@fundingcircle.com>
2023-12-06 22:14:41 +0000

    remove api key

--- rest_api/outputs.tf
+++ rest_api/outputs.tf
@@ -2,11 +2,6 @@ output "invoke_url" {
   value = aws_api_gateway_deployment.my_rest_api.invoke_url
 }
 
-output "api_key_value" {
-  value     = aws_api_gateway_api_key.api_key_1.value
-  sensitive = true
-}
-
 output "execution_arn" {
   value = aws_api_gateway_rest_api.default.execution_arn
 }
