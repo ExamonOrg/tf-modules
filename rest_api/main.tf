a82001f95b7222fe2f30d0de99d86c18d7937999
Jarrod Folino <jarrod.folino@fundingcircle.com>
2023-12-06 22:09:57 +0000

    remove api key

--- rest_api/main.tf
+++ rest_api/main.tf
@@ -28,36 +28,3 @@ resource "aws_api_gateway_stage" "example" {
   stage_name           = var.stage_name
   xray_tracing_enabled = var.xray_tracing_enabled
 }
-
-resource "aws_api_gateway_usage_plan" "example" {
-  name         = "my-usage-plan"
-  description  = "my description"
-  product_code = "MYCODE"
-
-  api_stages {
-    api_id = aws_api_gateway_rest_api.default.id
-    stage  = aws_api_gateway_stage.example.stage_name
-  }
-
-  quota_settings {
-    limit  = 1000
-    offset = 0
-    period = "DAY"
-  }
-
-  throttle_settings {
-    burst_limit = 10
-    rate_limit  = 20
-  }
-}
-
-resource "aws_api_gateway_api_key" "api_key_1" {
-  name = "api_key_1"
-}
-
-resource "aws_api_gateway_usage_plan_key" "main" {
-  key_id        = aws_api_gateway_api_key.api_key_1.id
-  key_type      = "API_KEY"
-  usage_plan_id = aws_api_gateway_usage_plan.example.id
-}
-
